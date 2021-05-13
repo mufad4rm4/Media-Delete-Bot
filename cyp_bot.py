ae274c86735362fef62da0015daba961e9849345
alicenewbot <81892911+paavampayyan@users.noreply.github.com>
2021-05-13 09:04:13 +0530

    update

--- cyp_bot.py
+++ cyp_bot.py
@@ -1,17 +1,19 @@
 #code by @nousername_psycho
 
 from pyrogram import Client, filters
-from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import FloodWait
 import time
 import random
 import os
+import asyncio
 
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
 bot_token = os.environ["BOT_TOKEN"]
-s_channel = os.environ["S_CHANNEL_ID"]
-sc_channel = int(os.environ["SEC_CHANNEL_ID"])
+
+time = []
+admins = []
 
 start_img = [
     "https://telegra.ph/file/bf15b6794e857518655d9.jpg",
@@ -28,36 +30,37 @@ print("bot starting")
 
 @cyp.on_message(filters.command(['start']) & filters.private)
 def start(client, message):
-    message.reply_photo(photo=random.choice(start_img), caption= "💣 അധോലോകം💣",
-                     reply_markup=InlineKeyboardMarkup(
-
-                            [
-                                [  # First row
-                                    InlineKeyboardButton(  # Generates a callback query when pressed
-                                        "Join Now",
-                                        url="https://t.me/Adholokam_Official"
-                                    )
-                                ]
-                            ]
+    message.reply_photo(photo=random.choice(start_img),
+                        caption= "💣 അധോലോകം💣",
+                        reply_markup=InlineKeyboardMarkup(
+                                [[InlineKeyboardButton("Join Now",url="https://t.me/Adholokam_Official")]])
                         )
-                        )
-
 
     return
-
 
 @cyp.on_message(filters.photo | filters.video | filters.document)
-def video_filter(client, message):
+async def media_files(client, message):
     chat_id = message.chat.id
     video_id = message.message_id
-    #============================ main()
-    first = message.copy(chat_id=s_channel, caption= "@neela_kkuyil")
-    second = message.copy(chat_id=sc_channel, caption= "@neela_kkuyil")
-    #============================ done()
-    time.sleep(15)
-    cyp.delete_messages(chat_id=chat_id, message_ids=video_id)
-    time.sleep(3570)
-    first.delete()
+
+    await asyncio.sleep(15)
+    try:
+        await cyp.delete_messages(chat_id=chat_id, message_ids=video_id)
+    except FloodWait as e:
+        time.sleep(e.x)
+        await cyp.delete_messages(chat_id=chat_id, message_ids=video_id)
+        
     return
 
+@cyp.on_message(filters.command('st') & filters.group)
+def set_time(__, message):
+    user_id = message.from_user.id
+    for member in cyp.get_chat_members(chat_id=message.chat.id, filter="administrators"):
+        admin = member.user.id
+        admins.append(admin)
+
+    if user_id in admins:
+        message.reply_text("ok good. pass time in seconds")
+        
+    
 cyp.run()
